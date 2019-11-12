f26a8fda5b51a4b066b4bdb0ea84da538c390472
LuisSanchez-Dev <luis.sanchez.dev@hotmail.com>
2019-11-12 02:07:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <h1 align="center">TheNewTTS</h1>
+
+  <p align="center">
+    The best Chat to Speech for Twitch, Mixer and Youtube streams for Streamlabs Chatbot
+    <br />
+    <a href="https://github.com/LuisSanchez-Dev/TheNewTTS/archive/master.zip">Download</a>
+    ·
+    <a href="https://github.com/LuisSanchez-Dev/TheNewTTS/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/LuisSanchez-Dev/TheNewTTS/issues">Request Feature</a>
+  </p>
+</p>
+
+## Table of Contents
+
+* [About the Project](#about-the-project)
+* [Getting Started](#getting-started)
+  * [Prerequisites](#prerequisites)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [Contact](#contact)
+
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+I have looked around for an easy to use and customizable text to speech script for Streamlabs Chatbot. The ones using the default voices installed in Windows sounded awful and other scripts weren't customizable enough.
+With this at hand, I took the time to build a better text to speech script for Streamlabs Chatbot that uses Google voices and is way more customizable and easy to use.
+
+Why is it better?
+* Plug and play - Just ⬇️ download, 📁 import, ✔️ enable and you are ready to go!
+* All the configuration you need to trigger the TTS:
+  * Command - !say, !tts, !speak or whatever your heart desires
+  * Global and user cooldown - Limit the use for each user or/and all users with cooldown
+  * Permission - Should everyone use the command? Regulars? Subs? you pick!
+  * Cost - Set a cost for your command and spend Streamlabs Chatbot currency
+* Don't want to use commands? Enable ✔️*Read ALL chat* and read your entire chat!
+* Make the voice unique with these configurations:
+  * Language - Choose between 33 languages
+  * Pitch - Custom pitch from super deep to extremely high pitched voice
+  * Speed - Set your own talking speed from 75% to 250%
+  * Volume - Volume separated from the Streamlabs Chatbot settings to meet your sound-fu requirements
+
+Thanks to [@Tecno_Diana](https://www.twitch.tv/tecno_diana/) for coming up with the requirement.
+
+## Getting Started
+
+Setting this up so you can use it is super straightforward.
+
+### Prerequisites
+
+Have an installation of Streamlabs Chatbot, already logged in to your accounts.
+* [Download Streamlabs Chatbot](https://streamlabs.com/chatbot)
+
+Follow this tutorial to prepare your Streamlabs Chatbot installation to accept scripts.
+* [[Streamlabs Chatbot] Scripts Explained by Castorr91](https://www.youtube.com/watch?v=l3FBpY-0880&t=3s)
+### Installation
+
+1. Download the latest version of the script [**here**](https://github.com/LuisSanchez-Dev/TheNewTTS/archive/master.zip).
+2. If you haven't already, open your Streamlabs Chatbot and log in to your Streamer and Bot accounts.
+3. On the left side, wait for the `Scripts` tab to pop up and click it.
+4. On the top right corner of the window, next to the reload button is an import script button (Arrow pointing right to a box) and select the script downloaded before.
+5. You will receive a message box confirming the import, accept it.
+6. The window will update and show the `TheNewTTS` script, make sure to ✔️ enable the script on the right hand side.
+7. Click on the `TheNewTTS` name to see the configuration pane.
+
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+After installing you can send the following chat message to hear the result:
+```
+!tts Hello world, this must be working now!
+```
+You can go to the configuration pane as specified in the installation to change the configuration for the command and for the text to speech voice.
+
+## Contact
+
+* Discord - luissanchezdev#6247
+* Fiverr - [luissanchezdev](https://fiverr.com/luissanchezdev)
+* [+52 81 1716 1989](tel:+528117161989)
+* luis.sanchez.dev@hotmail.com
+
+Remember to join the [Streamlabs Chatbot Discord server](https://discordapp.com/invite/S2d4KGg) for sfx, scripts, commands and a lot more!
---
+++ TheNewTTS_StreamlabsSystem.py
@@ -0,0 +1,195 @@
+#--------------------#
+#  Import Libraries  #
+#--------------------#
+import re
+import os
+import sys
+import clr
+import time
+import json
+import codecs
+import System
+from collections import deque
+
+clr.AddReference("System.Web")
+from System.Web import HttpUtility
+from System.Net import WebClient
+
+#---------------------------------#
+#  [Required] Script Information  #
+#---------------------------------#
+Description = "Text to speech with Google translate voice"
+ScriptName = "TheNewTTS"
+Creator = "LuisSanchezDev"
+Version = "1.0"
+Website = "https://www.fiverr.com/luissanchezdev"
+
+#---------------------------
+#  Define Global Variables
+#---------------------------
+global PATH, TEMP_MP3, CACHE_MP3, LOCK_FILE, FINISH_FILE, CONFIG_FILE
+PATH = os.path.dirname(os.path.realpath(__file__))
+TEMP_MP3 = os.path.join(PATH,"cache","tts2.mp3")
+CACHE_MP3 = os.path.join(PATH,"cache","tts.mp3")
+LOCK_FILE = os.path.join(PATH,"cache","lock")
+FINISH_FILE = os.path.join(PATH,"cache","finished")
+CONFIG_FILE = os.path.join(PATH,"config.json")
+
+global SETTINGS, TEXTS_QUEUE
+SETTINGS = {}
+TEXTS_QUEUE = deque()
+
+global last_check_period
+last_check_period = 0
+
+#----------------------------------------------------#
+#  [Required] Initialize Data (Only called on load)  #
+#----------------------------------------------------#
+def Init():
+  global SETTINGS, CONFIG_FILE
+  to_remove = [CACHE_MP3, LOCK_FILE, FINISH_FILE, TEMP_MP3]
+  for file in to_remove: os.popen('del "{0}"'.format(file))
+  
+  try:
+    with codecs.open(CONFIG_FILE, encoding="utf-8-sig", mode='r') as file:
+      SETTINGS = json.load(file, encoding="utf-8-sig")
+  except:
+    SETTINGS = {
+      "read_all_text": False,
+      "command": "!tts",
+      "permission": "Everyone",
+      "cooldown": 0,
+      "user_cooldown": 0,
+      "cost": 0,
+      "msg_permission": "You don't have enough permissions to use this command.",
+      "msg_cooldown": "This command is still on cooldown!",
+      "msg_user_cooldown": "You need to wait before using this command again.",
+      "msg_cost": "You don't have enough money!",
+      "lang": "English (US) [en-US]",
+      "pitch": 100,
+      "speed": 100,
+      "volume": 90
+    }
+  SETTINGS["lang"] = re.match(r"^.*\[(.+)\]", SETTINGS["lang"]).groups()[0]
+  SETTINGS["pitch"] /= 100.0
+  SETTINGS["speed"] /= 100.0
+  SETTINGS["volume"] /= 100.0
+
+#----------------------------------------------#
+#  [Required] Execute Data / Process messages  #
+#----------------------------------------------#
+def Execute(data):
+  if data.IsChatMessage():
+    if SETTINGS["read_all_text"]:
+      TEXTS_QUEUE.append(data.Message)
+      return
+    elif data.GetParam(0) == SETTINGS["command"]:
+      if not Parent.HasPermission(data.User, SETTINGS["permission"], ""):
+        Parent.SendStreamMessage(SETTINGS["msg_permission"])
+        return
+      if SETTINGS["user_cooldown"] and Parent.GetUserCooldownDuration(ScriptName, SETTINGS["command"], data.User):
+        Parent.SendStreamMessage(SETTINGS["msg_user_cooldown"])
+        return
+      if SETTINGS["cooldown"] and Parent.GetCooldownDuration(ScriptName, SETTINGS["command"]):
+        Parent.SendStreamMessage(SETTINGS["msg_cooldown"])
+        return
+      if not Parent.RemovePoints(data.User, data.UserName, SETTINGS["cost"]):
+        Parent.SendStreamMessage(SETTINGS["msg_cost"])
+        return
+      if not data.GetParam(1):
+        Parent.SendStreamMessage("You need to specify a message")
+        return
+      text = ' '.join(data.Message.split(' ')[1:])
+      TEXTS_QUEUE.append(text)
+      Parent.AddCooldown(ScriptName, SETTINGS["command"], SETTINGS["cooldown"])
+      Parent.AddUserCooldown(ScriptName, SETTINGS["command"], data.User, SETTINGS["user_cooldown"])
+      return
+
+#----------------------------------------------------------------------------------------------------#
+#   [Required] Tick method (Gets called during every iteration even when there is no incoming data)  #
+#----------------------------------------------------------------------------------------------------#
+def Tick():
+  global LOCK_FILE, FINISH_FILE, last_check_period
+  if time.time() - last_check_period >= 0.25:
+    try:
+      last_check_period = time.time()
+      if os.path.isfile(LOCK_FILE) or os.path.isfile(FINISH_FILE):
+        if os.path.isfile(FINISH_FILE):
+          os.popen('del "{0}" & del "{1}"'.format(
+            LOCK_FILE, FINISH_FILE
+          ))
+          os.remove(CACHE_MP3)
+          os.rename(TEMP_MP3, CACHE_MP3)
+          Parent.PlaySound(CACHE_MP3, 100)
+          TEXTS_QUEUE.popleft()
+          return
+        else:
+          return
+      if os.path.isfile(CACHE_MP3):
+        os.popen('del "{0}"'.format(CACHE_MP3))
+        last_check_period = time.time()
+        return
+      if len(TEXTS_QUEUE) > 0:
+        text = TEXTS_QUEUE[0]
+        download_tts(CACHE_MP3, text)
+        filter_audio(CACHE_MP3)
+    except Exception as e:
+      Parent.Log(ScriptName, "Please report this to the github page.\nERROR: " + str(e))
+    return
+
+#-----------------------------------------------------------------------------------------------------#
+#  [Optional] Reload Settings (Called when a user clicks the Save Settings button in the Chatbot UI)  #
+#-----------------------------------------------------------------------------------------------------#
+def ReloadSettings(jsonData):
+  global SETTINGS, TEXTS_QUEUE
+  Init()
+  clear_queue()
+  TEXTS_QUEUE.append("Configuration updated successfuly") 
+
+#-----------------------------------------------------------------------#
+#  [TheNewTTS] Changes the pitch, speed and volume of downloaded mp3  #
+#-----------------------------------------------------------------------#
+def filter_audio(file_path):
+  global SETTINGS, PATH, LOCK_FILE, FINISH_FILE, TEMP_MP3
+  with open(LOCK_FILE,"w") as f: f.write(" ")
+  
+  commands = [
+    'cd "'+PATH+'"',
+    'ffmpeg.exe -i "{0}" -af asetrate=24000*{1},atempo={2}/{1},aresample=48000,volume={3} "{4}" -y'.format(
+      file_path, SETTINGS["pitch"], SETTINGS["speed"], SETTINGS["volume"], TEMP_MP3
+    ),
+    'echo 1 > "' + FINISH_FILE + '"'
+  ]
+  os.popen(" & ".join(commands))
+  return
+
+#-----------------------------------------------------------------------------------------#
+#  [TheNewTTS] Download from Google Translate voice generator using defined TTS language  #
+#-----------------------------------------------------------------------------------------#
+def download_tts(file_path,text):
+  global SETTINGS
+  with WebClient() as wc:
+    url = "https://translate.google.com/translate_tts?ie=UTF-8&q={0}&tl={1}&client=tw-ob".format(
+      HttpUtility.UrlEncode(text), SETTINGS["lang"]
+    )
+    wc.Headers["Referer"] = "http://translate.google.com/"
+    wc.Headers["User-Agent"] = "stagefright/1.2 (Linux;Android 5.0)"
+    wc.DownloadFile(url,file_path)
+    return
+
+#---------------------------#
+#  [TheNewTTS] Clear queue  #
+#---------------------------#
+def clear_queue():
+  global TEXTS_QUEUE
+  while len(TEXTS_QUEUE) > 0: TEXTS_QUEUE.pop()
+
+#-------------------------------#
+#  [TheNewTTS] UI Link buttons  #
+#-------------------------------#
+def donate():
+  os.startfile("https://streamlabs.com/luissanchezdev/tip")
+def open_contact_me():
+  os.startfile("www.fiverr.com/luissanchezdev/makea-custom-streamlabs-chatbot-script")
+def open_readme():
+  os.startfile("https://github.com/LuisSanchez-Dev/TheNewTTS")
